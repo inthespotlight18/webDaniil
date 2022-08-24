9b75385200a6fc4dcb17c0890c588740037cfb3f
inthespotlight17 <64893014+inthespotlight17@users.noreply.github.com>
2022-08-24 11:55:58 -0700

    DZ220824 - fixed project
    Changed .exe files in service_intall.cmd

--- webDaniil/bin/Release/service_install.cmd
+++ webDaniil/bin/Release/service_install.cmd
@@ -1,5 +1,5 @@
 echo "Run as Admin"
 echo "DZ220822 - add servicename"
 path c:\windows\microsoft.net\framework\v4.0.30319
-InstallUtil /i /ServiceName=DZ_webDaniil_Service WindowsServiceCS.exe
-rem InstallUtil /u /ServiceName=DZ_webDaniil_Service WindowsServiceCS.exe
+InstallUtil /i /ServiceName=DZ_webDaniil_Service webDaniil.exe
+rem InstallUtil /u /ServiceName=DZ_webDaniil_Service webDaniil.exe
